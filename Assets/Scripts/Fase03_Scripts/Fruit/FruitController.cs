0d865aab9406476f219f44354edb51c7abba2422
Gustavo <149015908+Gustavoogalo@users.noreply.github.com>
2026-01-11 18:57:32 -0300

    feat - Fruits change appearance according to the modifications.

--- Assets/Scripts/Fase03_Scripts/Fruit/FruitController.cs
+++ Assets/Scripts/Fase03_Scripts/Fruit/FruitController.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Fase03_Scripts.Basket;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace Fase03_Scripts.Fruit
 {
@@ -30,14 +34,53 @@ namespace Fase03_Scripts.Fruit
         Pear,
         Banana
     }
-    
+
+    [Serializable]
+    public class FruitVisualConfig
+    {
+        public FruitType fruitType;
+        public Sprite sprite;
+    }
+
+    [Serializable]
+    public class SizeConfig
+    {
+        public TamanhoModification tamanho;
+        public float scale;
+    }
+
+    [Serializable]
+    public class ColorConfig
+    {
+        public CorModification colorEnum;
+        public Color colorValue;
+    }
+
+    [Serializable]
+    public class FormaConfig
+    {
+        public FormaModification formaEnum;
+        public Sprite maskSprite;
+    }
     public class FruitController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
+        [Header("Settings")]
         [SerializeField] private FruitType _fruitType;
         [SerializeField] private TamanhoModification _tamanho;
         [SerializeField] private FormaModification _forma;
         [SerializeField] private CorModification _cor;
 
+        [Header("Visual Customization")] [SerializeField]
+        private Image _imageComponent;
+
+        [SerializeField] private Image _maskImage;
+
+        [SerializeField] private List<FruitVisualConfig> _typeConfigs;
+        [SerializeField] private List<FormaConfig> _formaConfigs;
+        [SerializeField] private List<SizeConfig> _sizeConfigs;
+        [SerializeField] private List<ColorConfig> _colorConfigs;
+        
+        
         public bool isRef = false;
 
         private Vector3 _startPos;
@@ -51,12 +94,14 @@ namespace Fase03_Scripts.Fruit
         private void Awake()
         {
             _canvasGroup = GetComponent<CanvasGroup>();
+            if(_imageComponent == null) _imageComponent = GetComponent<Image>();
+            UpdateVisuals();
+            if (isRef)
+            {
+                _imageComponent.color = new Color(
+                    _imageComponent.color.r, _imageComponent.color.g, _imageComponent.color.b, 0.5f);
+            }
         }
-        
-        // public bool VerifyFruit(FruitType type,TamanhoModification tamanho, FormaModification forma, CorModification cor)
-        // {
-        //     return (type == _fruitType && tamanho == _tamanho && forma == _forma && cor == _cor);
-        // }
         
         public void SetFruitModifications(FruitType type,TamanhoModification tamanho, FormaModification forma, CorModification cor)
         {
@@ -64,6 +109,32 @@ namespace Fase03_Scripts.Fruit
             _tamanho = tamanho;
             _forma = forma;
             _cor = cor;
+            UpdateVisuals();
+        }
+
+        private void UpdateVisuals()
+        {
+            var typeCfg = _typeConfigs.FirstOrDefault(cfg => cfg.fruitType == _fruitType);
+            if(typeCfg != null && _imageComponent != null) _imageComponent.sprite = typeCfg.sprite;
+
+            var formaCfg = _formaConfigs.FirstOrDefault(cfg => cfg.formaEnum == _forma);
+            if(formaCfg != null && _maskImage != null) _maskImage.sprite = formaCfg.maskSprite;
+            
+            var sizeCfg = _sizeConfigs.FirstOrDefault(cfg => cfg.tamanho == _tamanho);
+            if(sizeCfg != null && _imageComponent != null) _imageComponent.rectTransform.localScale = new Vector3(sizeCfg.scale, sizeCfg.scale, 1);
+
+            ApplyColorModification();
+        }
+
+        private void ApplyColorModification()
+        {
+            if(_imageComponent == null) return;
+           
+            var coloCfg = _colorConfigs.FirstOrDefault(cfg => cfg.colorEnum == _cor);
+            Color targetColor = coloCfg != null ? coloCfg.colorValue : Color.white;
+
+            float alpha = isRef ? 0.5f : 1f;
+            _imageComponent.color = new Color(targetColor.r, targetColor.g, targetColor.b, alpha);
         }
 
         public void OnBeginDrag(PointerEventData eventData)
